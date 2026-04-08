f6b40eb539c975e0c7f6cf2dea27fb0f06d8f584
Hikari <nickhunter509@gmail.com>
2026-04-08 21:08:36 +0300

    Refactor environment variables and module exports
    Removed unused environment variables and restructured module exports.

--- set.js
+++ set.js
@@ -4,38 +4,38 @@ There's no need to configure this if you're deploying via Heroku — just set th
 
 const sessionName = 'session';
 const session = process.env.SESSION || '';
-const autobio = process.env.AUTOBIO || 'FALSE';
-const autolike = process.env.AUTOLIKE_STATUS || 'TRUE';
-const autoviewstatus = process.env.AUTOVIEW_STATUS || 'TRUE';
-const welcomegoodbye = process.env.WELCOMEGOODBYE || 'FALSE';
-const prefix = process.env.PREFIX || '';
 const appname = process.env.APP_NAME || '';
 const herokuapi = process.env.HEROKU_API;
-const gptdm = process.env.GPT_INBOX || 'FALSE';
-const mode = process.env.MODE || 'PRIVATE';
-const anticall = process.env.AUTOREJECT_CALL || 'TRUE';
 const botname = process.env.BOTNAME || '𝐁𝐋𝐀𝐂𝐊-𝐌𝐃 𝐁𝐎𝐓';
-const antibot = process.env.ANTIBOT || 'FALSE';
-const author = process.env.STICKER_AUTHOR ||'𝗕𝗢𝗧';
+const author = process.env.STICKER_AUTHOR || '𝗕𝗢𝗧';
 const packname = process.env.STICKER_PACKNAME || '𝐁𝐋𝐀𝐂𝐊𝐌𝐄𝐑𝐂𝐇𝐀𝐍𝐓';
-const antitag = process.env.ANTITAG || 'TRUE';
 const dev = process.env.DEV || '254114283550,254105309571';
-const menulink = process.env.MENU_LINK || 'https://files.catbox.moe/jxxwms.jpeg';
-const menu = process.env.MENU_TYPE || 'IMAGE';
-const DevRaven = dev.split(",");
-const badwordkick = process.env.BAD_WORD_KICK || 'FALSE';
-const bad = process.env.BAD_WORD || 'fuck';
-const autoread = process.env.AUTOREAD || 'FALSE';
-const antidel = process.env.ANTIDELETE || 'TRUE';
+const owner = dev.split(",");
+const bad = process.env.BAD_WORD || 'fuck,umbwa,kuma,pussy,slut,bitch,cock,stupid';
 const admin = process.env.ADMIN_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗿𝗲𝘀𝗲𝗿𝘃𝗲𝗱 𝗳𝗼𝗿 𝗔𝗱𝗺𝗶𝗻𝘀!';
 const group = process.env.GROUP_ONLY_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝗚𝗿𝗼𝘂𝗽𝘀!';
 const botAdmin = process.env.BOT_ADMIN_MSG || '𝗜 𝗻𝗲𝗲𝗱 𝗔𝗱𝗺𝗶𝗻 𝗽𝗿𝗲𝘃𝗶𝗹𝗲𝗱𝗴𝗲𝘀!';
 const NotOwner = process.env.NOT_OWNER_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝘁𝗵𝗲 𝗼𝘄𝗻𝗲𝗿!';
-const wapresence = process.env.WA_PRESENCE || 'recording';
-const antilink = process.env.ANTILINK || 'TRUE';
 const mycode = process.env.CODE || '254';
-const antiforeign = process.env.ANTIFOREIGN || 'TRUE';
 const port = process.env.PORT || 10000;
-const antilinkall = process.env.ANTILINK_ALL || 'TRUE';
+const databaseUrl = process.env.DATABASE_URL || '';
 
-module.exports = { session, sessionName, autobio, author, packname, dev, DevRaven, badwordkick, bad, mode, group, NotOwner, botname, botAdmin, antiforeign, menu, autoread, antilink, admin, mycode, antilinkall, anticall, antitag, antidel, wapresence, welcomegoodbye, antibot, herokuapi, prefix, port, gptdm, appname, autolike, autoviewstatus };  
+module.exports = {
+  session,
+  sessionName,
+  author,
+  packname,
+  dev,
+  owner,
+  bad,
+  group,
+  NotOwner,
+  botname,
+  botAdmin,
+  admin,
+  mycode,
+  herokuapi,
+  port,
+  appname,
+  databaseUrl
+};
