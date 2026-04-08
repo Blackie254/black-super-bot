2118fede117d60aff15ec14a6c97a700d8683306
Hikari <nickhunter509@gmail.com>
2026-04-08 21:39:43 +0300

    Refactor welcome/goodbye logic and bot name import
    Updated welcome and goodbye message conditions to check for 'on' instead of 'TRUE'. Replaced environment variable for bot name with a required module.

--- action/events.js
+++ action/events.js
@@ -1,9 +1,10 @@
-const welcomegoodbye = process.env.WELCOMEGOODBYE || 'FALSE'; 
-const botname = process.env.BOTNAME || '𝑩𝑳𝑨𝑪𝑲𝑴𝑨𝑪𝑯𝑨𝑵𝑻';
+const botname = require('../set');
+const fetchSettings = require('../database/fetchSettings');
 
 const Events = async (client, Nick) => {
     
     try {
+        const welcomegoodbye = await fetchSettings();
         let metadata = await client.groupMetadata(Nick.id);
         let participants = Nick.participants;
         let desc = metadata.desc || "No Description";
@@ -22,7 +23,7 @@ const Events = async (client, Nick) => {
                 let userName = num;
 
                 let Welcometext = `@${userName.split("@")[0]} Holla👋,\n\nWelcome to ${metadata.subject}.\n\nYou might want to read group description,\nFollow group rules to avoid being removed.\n\n ${botname} 2025.`;
-                if (welcomegoodbye === 'TRUE') {
+                if (welcomegoodbye === 'on') {
                     await client.sendMessage(Nick.id, {
                         image: { url: dpuser },
                         caption: Welcometext,
@@ -33,7 +34,7 @@ const Events = async (client, Nick) => {
                 let userName2 = num;
 
                 let Lefttext = `@${userName2.split("@")[0]} Goodbye we shall miss you😔.\n\nAnyway Goodbye .`;
-                if (welcomegoodbye === 'TRUE') {
+                if (welcomegoodbye === 'on') {
                     await client.sendMessage(Nick.id, {
                         image: { url: dpuser },
                         caption: Lefttext,
